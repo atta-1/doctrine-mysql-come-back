e0ef8d49a2ab5c45bdd9fb48158586740a48b173
jlonom <3850185@gmail.com>
2024-03-08 11:52:27 +0200

    Feat: pms_296_fix_deprecations - Rewrite connection method

--- src/Connection.php
+++ src/Connection.php
@@ -5,4 +5,23 @@ namespace Facile\DoctrineMySQLComeBack\Doctrine\DBAL;
 class Connection extends \Doctrine\DBAL\Connection
 {
     use ConnectionTrait;
+
+    public function connect()
+    {
+        if ($this->_conn !== null) {
+            return false;
+        }
+
+        try {
+            $this->_conn = $this->_driver->connect($this->getParams());
+        } catch (\Doctrine\DBAL\Driver\Exception $e) {
+            throw $this->convertException($e);
+        }
+
+        if (!$this->isAutoCommit()) {
+            $this->beginTransaction();
+        }
+
+        return true;
+    }
 }
